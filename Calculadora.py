9ac0fba3a936d6405e8132170937fb8e88216a39
RojasHenry <henry.red1@hotmail.com>
2017-05-24 13:05:04 -0500

    Claculadora- Tkinter

--- Calculadora.py
+++ Calculadora.py
@@ -94,4 +94,47 @@ botones()
 
 #mainloop()#Bucle de todo el codigo
 
+########################################################################
+#####################DAVID CASA CODIGO CALCULADORA######################
+
+
+from tkinter import *
+
+def btnClick(numbers):
+    global operator
+    operator = operator + str(numbers)
+    text_Input.set(operator)
+
+def btnClearDisplay():
+    global operator
+    operator=""
+    text_Input.set("")
+
+cal = Tk()
+cal.title('calculator')
+operator = ''
+text_Input = StringVar()
+
+
+txt_Display = Entry(cal,textvariable=text_Input, bd = 30, insertwidth = 4,
+    ).grid(columnspan=4)
+btnast = Button(cal,bd = 10, text = '*', command=lambda:btnClick('*')).grid(row=1,column=0)
+btn = Button(cal,bd = 10, text = '+', command=lambda:btnClick('+')).grid(row=1,column=1)
+btn7 = Button(cal,bd = 10, text = '-', command=lambda:btnClick('-')).grid(row=1,column=2)
+btn7 = Button(cal,bd = 10, text = '7', command=lambda:btnClick(9)).grid(row=2,column=0)
+btn7 = Button(cal,bd = 10, text = '8', command=lambda:btnClick(8)).grid(row=2,column=1)
+btn7 = Button(cal,bd = 10, text = '9', command=lambda:btnClick(7)).grid(row=2,column=2)
+btn7 = Button(cal,bd = 10, text = '4', command=lambda:btnClick(6)).grid(row=3,column=0)
+btn7 = Button(cal,bd = 10, text = '5', command=lambda:btnClick(5)).grid(row=3,column=1)
+btn7 = Button(cal,bd = 10, text = '6', command=lambda:btnClick(4)).grid(row=3,column=2)
+btn7 = Button(cal,bd = 10, text = '1', command=lambda:btnClick(3)).grid(row=4,column=0)
+btn7 = Button(cal,bd = 10, text = '2', command=lambda:btnClick(2)).grid(row=4,column=1)
+btn7 = Button(cal,bd = 10, text = '3', command=lambda:btnClick(1)).grid(row=4,column=2)
+btn7 = Button(cal,bd = 10, text = '0', command=lambda:btnClick(0)).grid(row=4,column=2)
+btn7 = Button(cal,bd = 10, text = 'C', command=btnClearDisplay).grid(row=1,column=3)
+btn7 = Button(cal,bd = 10, text = '=', command=lambda:btnClick(1)).grid(row=4,column=2)
+
+
+cal.mainloop()
+
 

34380a63a8262fa9757d4dad4093b509e7f74031
Geovanny Dias <geova_jd@hotmail.com>
2017-06-06 00:36:42 -0500

    Modificacion Interfaz Grafica

--- CalculadoraCientifica_EPN.py
+++ CalculadoraCientifica_EPN.py
@@ -1,130 +1,200 @@
+#eval() and exec() funciones predefinidas
+#v1=Toplevel(root) Ventana Hija #v1.withdraw()  # oculta v1
+#https://answers.microsoft.com/es-es/windows/forum/windows_7-windows_programs/funcion-de-los-botones-ms-m-m-mr-mc-ce-y-c-de-la/5caaf4b2-385a-e011-8dfc-68b599b31bf5
+#=========================================================================
+from distutils import command
+from tkinter import *
+from math import*
 
-##############################################################
-######################CALCULADORA#############################
+root = Tk()
+root.config(bg = "black")#bg = "lavender"
+root.geometry("443x323+280+200")#Tamaño ventada + Coordenadas de posicion X,Y
+root.maxsize(440, 320)
+root.minsize(440, 320)
+root.title('Calculadora Científica - EPN')
+                                #PANELES
+#==========================================================================================
+frame = Frame(root, bd=4, relief="groove",bg = "black")#gray, dark red, black
+frame.pack(fill=BOTH, padx=5, pady=5) #side = BOTTOM
 
-from tkinter import*
-import math
+calTop = Frame(frame, bd=4, relief="groove",bg = "black")
+calTop.pack(fill=BOTH, padx=5, pady=5)
 
-cal = Tk()
-op = ''
-x =''
-ver = False
+calLeft = Frame(frame, bd=4, width=500, height=300, relief="groove",bg = "black")
+calLeft.pack(fill=BOTH, padx=5, pady=5, ipadx=1, ipady=1, side = LEFT)
 
-cal.geometry("255x310+300+250")
-cal.title('Calculator Científica - EPN')
-cal.resizable(width=False, height=False)
-cal.config(bg="blue")
+calRight = Frame(frame, bd=4, width=500, height=300, relief="groove",bg = "black")
+calRight.pack(fill=BOTH, padx=5, pady=5, ipadx=1, ipady=1,side = RIGHT)
+#==========================================================================================
 
-operator = ''
+operador = ''
 text_Input = StringVar()
-def reiniciar():
-    ver=True
-    op = ""
-    operator1 = ""
-    operator = ""
+p = 0
+validar = False
+evaluarFun = False
+validarMemoria = False
+memoria = ''
 
-    
-def otr(operator1):
-    global op
-    global ver
-    ver = True
-    op = operator1
-    text_Input.set(operator1)
-    
-      
-def BtnOperacion(numbers):
-    global operator
-    operator = operator + str(numbers)
-    text_Input.set(op + operator)
-    
+def btnClick(tecla):
+    global operador
+    global validar
+    if validar:
+        if tecla != '+' and tecla != '-' and tecla != '*' and tecla != '/'\
+                and tecla != '**2' and tecla != '**3':
+            btnClearDisplay()
+    operador += str(tecla)#Forma una cadena de caracteres
+    text_Input.set(operador)
+    validar = False
 
-def BorrarDatos():
-    global operator
-    operator1 = ''
-    operator = ""
-    text_Input.set("")
-    op = ""
-    ver = False
 
+def btnClearDisplay():
+    global operador
+    operador = ''
+    text_Input.set('')
+
+#==========================================================================================
+                                #FUNCIONES ALTERNATIVAS
+#==========================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#==========================================================================================
+#==========================================================================================
 def btnResultado():
-    global operator
-    x = "sen("
-    y = "cos("
-    z = "tan("
-    a = "tan-1("
-    b = "cos-1("
-    c = "sen-1("
-    if ver == True:
-        if op == x:
-            s=str(math.sin(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        elif op ==y :
-            s=str(math.cos(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        elif op == z:
-            s=str(math.tan(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        elif op == a:
-            s = str(math.atan(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        elif op == b:
-            s = str(math.acos(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        elif op == c:
-            s = str(math.asin(math.radians(int(operator))))
-            text_Input.set(s)
-            operator = ""
-        
-    else:
-        s=str(eval(operator))
-        text_Input.set(s)
+    global validar
+    global evaluarFun
+    validar = True
+    try:
+        global operador
+        if operador == "":
+            btnClearDisplay()
+        else:
+            print(evaluarFun)
+            npa = operador.count('(')
+            npc = operador.count(')')
+            np = npa-npc
+            if np !=0:
+                if evaluarFun == False:
+                    char = '*('
+                    operador = operador.split('(')
+                    operador = char.join(operador)
+                    if operador[0] == '*':
+                        operador = operador.lstrip(operador[0])
+                    for i in range(np):
+                        operador +=')'
+                else:
+                    for i in range(np):
+                        operador +=')'
+            resultado = str(eval(operador))
+            if resultado == "()":
+                resultado = ""
+            text_Input.set(resultado)
+            operador = resultado
+            evaluarFun = False
 
-        operator = ""
-    
-
-txt_Display = Entry(cal, textvariable = text_Input, bd = 20, insertwidth = 4).grid(columnspan=100)
-
-####################################################################################################
-
-btnaSie = Button(cal, bd =15 , text= 7, relief="groove",command=lambda:BtnOperacion(7)).grid(row=1,column=0)
-btnOch = Button(cal, bd =15 , text= 8, relief="groove",command=lambda:BtnOperacion(8)).grid(row=1,column=1)
-btnNue = Button(cal, bd =15 , text= 9, relief="groove",command=lambda:BtnOperacion(9)).grid(row=1,column=2)
-btnast = Button(cal, bd =15 , text= '*',relief="groove", command=lambda:BtnOperacion('*')).grid(row=1,column=3)
-btnDiv = Button(cal, bd =15 , text= '/', relief="groove",command=lambda:BtnOperacion('/')).grid(row=1,column=4)
+    except ZeroDivisionError:
+        text_Input.set("Error.!!No Se Puede Dividir Entre Cero...")
+        operador = ''
+    except SyntaxError:
+        text_Input.set("Syntax Error")
+        operador = ''
+    except TypeError:
+        text_Input.set("Type Error")
+        operador = ''
+    except ValueError:
+        text_Input.set("Mathematical Error")
+        operador = ''
+    except Exception:
+        text_Input.set("Error")
+        operador = ''
 
 
-####################################################################################################
 
-btnCuat = Button(cal, bd =15 , text= 4, relief="groove",command=lambda:BtnOperacion(4)).grid(row=2,column=0)
-btnCinc = Button(cal, bd =15 , text= 5, relief="groove",command=lambda:BtnOperacion(5)).grid(row=2,column=1)
-btnSei = Button(cal, bd =15 , text= 6, relief="groove",command=lambda:BtnOperacion(6)).grid(row=2,column=2)
-btnSum = Button(cal, bd =15 , text= '+',relief="groove", command=lambda:BtnOperacion('+')).grid(row=2,column=3)
-btnClear = Button(cal, bd =15 , text= 'C', relief="groove",command=BorrarDatos).grid(row=2,column=4)
+#==========================================================================================
+                                #BOTONES
+#==========================================================================================
+#DISPLAY
+txt_Display = Entry(calTop, font=('arial',14,'bold'), textvariable=text_Input, bd=15, insertwidth=4,
+                    state = DISABLED, width=34, justify=RIGHT).grid(columnspan=4)
+#BOTONES NUMEROS
+btn = Button(calLeft, bd=10, text='7', width=2, command=lambda: btnClick('7')).grid(row=0, column=0)
+btn = Button(calLeft, bd=10, text='8', width=2, command=lambda: btnClick('8')).grid(row=0, column=1)
+btn = Button(calLeft, bd=10, text='9', width=2, command=lambda: btnClick('9')).grid(row=0, column=2)
+btn = Button(calLeft, bd=10, text='4', width=2, command=lambda: btnClick('4')).grid(row=1, column=0)
+btn = Button(calLeft, bd=10, text='5', width=2, command=lambda: btnClick('5')).grid(row=1, column=1)
+btn = Button(calLeft, bd=10, text='6', width=2, command=lambda: btnClick('6')).grid(row=1, column=2)
+btn = Button(calLeft, bd=10, text='3', width=2, command=lambda: btnClick('3')).grid(row=2, column=0)
+btn = Button(calLeft, bd=10, text='2', width=2, command=lambda: btnClick('2')).grid(row=2, column=1)
+btn = Button(calLeft, bd=10, text='1', width=2, command=lambda: btnClick('1')).grid(row=2, column=2)
+btn = Button(calLeft, bd=10, text='0', width=2, command=lambda: btnClick('0')).grid(row=3, column=0)
+btn = Button(calLeft, bd=10, text='.', width=2, command=lambda: btnClick('.')).grid(row=3, column=1)
+btn = Button(calLeft, bd=10, text='√', width=2, command=lambda: btnClick('sqrt(')).grid(row=3, column=2)
+#BOTONES OPERADORES
+btn = Button(calLeft, bd=10, text='/', width=2, command=lambda: btnClick('/')).grid(row=0, column=3)
+btn = Button(calLeft, bd=10, text='*', width=2, command=lambda: btnClick('*')).grid(row=1, column=3)
+btn = Button(calLeft, bd=10, text='-', width=2, command=lambda: btnClick('-')).grid(row=2, column=3)
+btn = Button(calLeft, bd=10, text='+', width=2, command=lambda: btnClick('+')).grid(row=3, column=3)
+btn = Button(calLeft, bd=10, text='⌫', width=2, command=lambda: btnEliminarChar()).grid(row=4, column=0)
+btn = Button(calLeft, bd=10, text='CE', width=2, command=lambda: btnEliminarChar()).grid(row=4, column=1)
+btn = Button(calLeft, bd=10, text='C', width=2, command=lambda: btnClearDisplay()).grid(row=4, column=2)
+btn = Button(calLeft, bd=10, text='=', width=2, command=lambda: btnResultado()).grid(row=4, column=3)
+#==========================================================================================
+                                #BOTONES TRIGONOMETRICOS
+#==========================================================================================
+#BOTONES TRIGONOMETRICOS
+btn = Button(calRight, bd=10, text='(', width=4, command=lambda: btnParentesis('(')).grid(row=0, column=0)
+btn = Button(calRight, bd=10, text=')', width=4, command=lambda: btnParentesis(')')).grid(row=0, column=1)
+btn = Button(calRight, bd=10, text='%', width=4, command=lambda: btnPorcentaje('%')).grid(row=0, column=2)
 
-####################################################################################################
+btn = Button(calRight, bd=10, text='Sin', width=4, command=lambda: btnFunTrigonometrica('sin(radians(')).grid(row=1, column=0)
+btn = Button(calRight, bd=10, text='aSin', width=4, command=lambda: btnFunTrigonometrica('degrees(asin(')).grid(row=1, column=1)
+btn = Button(calRight, bd=10, text='X²', width=4, command=lambda: btnClick('**2')).grid(row=1, column=2)
 
-btnUn = Button(cal, bd =15 , text= 1, relief="groove",command=lambda:BtnOperacion(1)).grid(row=3,column=0)
-btnDos = Button(cal, bd =15 , text= 2,relief="groove", command=lambda:BtnOperacion(2)).grid(row=3,column=1)
-btnTres = Button(cal, bd =15 , text= 3,relief="groove", command=lambda:BtnOperacion(3)).grid(row=3,column=2)
-btnRes = Button(cal, bd =15, text= '-',relief="groove",command=lambda:BtnOperacion('-')).grid(row=3,column=3)
-btnPto = Button(cal, bd =15, text= '.',relief="groove", command=lambda:BtnOperacion('.')).grid(row=3,column=4)
-btnCer = Button(cal, bd =15 , text= 0,relief="groove", command=lambda:BtnOperacion(0)).grid(row=4,column=0)
+btn = Button(calRight, bd=10, text='Cos', width=4, command=lambda: btnFunTrigonometrica('cos(radians(')).grid(row=2, column=0)
+btn = Button(calRight, bd=10, text='aCos', width=4, command=lambda: btnFunTrigonometrica('degrees(acos(')).grid(row=2, column=1)
+btn = Button(calRight, bd=10, text='X³', width=4, command=lambda: btnClick('**3')).grid(row=2, column=2)
 
-####################################################################################################
-                                                                            
-btnSen = Button(cal, bd =7 , text= 'sin(',relief="groove", command=lambda:otr('sen(')).grid(row=4,column=1)
-btnCos = Button(cal, bd =7 , text= 'cos(',relief="groove", command=lambda:otr('cos(')).grid(row=4,column=2)
-btnTan = Button(cal, bd =7 , text= 'tan(',relief="groove", command=lambda:otr('tan(')).grid(row=4,column=3)
-btnTan = Button(cal, bd =7 , text= 'Sen-1(',relief="groove", command=lambda:otr('sen-1(')).grid(row=5,column=1)
-btnTan = Button(cal, bd =7 , text= 'Cos-1(',relief="groove", command=lambda:otr('cos-1(')).grid(row=5,column=2)
-btnTan = Button(cal, bd =7 , text= 'Tan-1(',relief="groove", command=lambda:otr('tan-1(')).grid(row=5,column=3)
-btnIgual = Button(cal, bd =10.5, text= '=', relief="groove",command=btnResultado).grid(row=4,column=4)
-cal.mainloop()
+btn = Button(calRight, bd=10, text='Tan', width=4, command=lambda: btnFunTrigonometrica('tan(radians(')).grid(row=3, column=0)
+btn = Button(calRight, bd=10, text='aTan', width=4, command=lambda: btnFunTrigonometrica('degrees(atan(')).grid(row=3, column=1)
+btn = Button(calRight, bd=10, text='X^y', width=4, command=lambda: btnClick('**')).grid(row=3, column=2)
+
+#BOTONES CONFIG
+btn = Button(calRight, bd=10, text='MC', width=4, command=lambda: btnMemoria('MC')).grid(row=0, column=3)
+btn = Button(calRight, bd=10, text='MR', width=4, command=lambda: btnMemoria('MR')).grid(row=1, column=3)
+btn = Button(calRight, bd=10, text='MS', width=4, command=lambda: btnMemoria('MS')).grid(row=2, column=3)
+btn = Button(calRight, bd=10, text='M+', width=4, command=lambda: btnMemoria('M+')).grid(row=3, column=3)
+
+btn = Button(calRight, bd=10, text='Log', width=4, command=lambda: btnFunTrigonometrica('log10(')).grid(row=4, column=0)
+btn = Button(calRight, bd=10, text='π', command=lambda: btnClick(pi), width=4).grid(row=4, column=1)
+btn = Button(calRight, bd=10, text='n!', width=4, command=lambda: btnFactorial()).grid(row=4, column=2)
+btn = Button(calRight, bd=10, text='M-', width=4, command=lambda: btnMemoria('M-')).grid(row=4, column=3)
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
 
 
 #LAS FUNCIONES SEN, COS, TAN, YA SE CALCULAN HAY QUE VALIDAR CUANDO CADA FUNCION TOMA VALOR DE
